9dcf4986b5cd11b19059ff539019b372de19205a
alemv18 <alemv18@hotmail.com>
2017-09-17 16:55:04 -0500

    Add event handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'; 
+
 const API_KEY = 'AIzaSyCV2T1e-JPxGeMSt4g2G79YcbUj5TIaxZ0';
 
-const App = () => <h1>Youtube project </h1>;
+const App = () => {
+ return (
+	<div>
+		<SearchBar />
+	</div>
+  );
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
