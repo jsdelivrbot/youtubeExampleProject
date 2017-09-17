429faaccd1c19fea6a5286641e5b634bd9392114
alemv18 <alemv18@hotmail.com>
2017-09-16 22:21:44 -0500

    Change to es6

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App =  function() {
-	return <h1> Youtube project </h1>;
-
-}
+const App = () => <h1>Youtube project </h1>;
 
 ReactDOM.render(<App />, document.querySelector('.container'));
