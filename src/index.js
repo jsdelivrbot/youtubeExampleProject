35e1daaa01ba38a5f2b97134e7967dee70078cf0
alemv18 <alemv18@hotmail.com>
2017-09-16 22:30:50 -0500

    Add api_key for youtube API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyCV2T1e-JPxGeMSt4g2G79YcbUj5TIaxZ0';
+
 const App = () => <h1>Youtube project </h1>;
 
 ReactDOM.render(<App />, document.querySelector('.container'));
