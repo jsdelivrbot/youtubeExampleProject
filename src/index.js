a896ece5a102545a3115327a7402f2c4a8bfba8d
Alejandra Muñoz <alemv18@gmail.com>
2017-12-15 15:16:55 -0600

    Add videolist component and add it on app component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {Component}from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
+import VideoList from './components/video_list.js'
 import SearchBar from './components/search_bar'; 
 
 const API_KEY = 'AIzaSyBvEMuDzRQIH5vFTicJmHEJ2Le-SrKXK8U';
 
-YTSearch({key: API_KEY,term:'surfboards'}, function (data) {
-	console.log(data);
-});
 
-const App = () => {
- return (
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state= { videos: [] };
+
+		YTSearch({key: API_KEY,term:'surfboards'}, (videos) => {
+		this.setState({ videos });
+		//This is the same as this.setState({videos:videos});
+  });
+}
+	render () {
+		return (
 	<div>
 		<SearchBar />
+		<VideoList videos={this.state.videos}/>
 	</div>
   );
+ }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
